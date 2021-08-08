1a5001d9c58f6eb0899d1f422360c2d902a1a1c0
marcelinoSousa <marcelino.feliciano@hotmail.com>
2021-08-08 11:00:43 -0400

    Criação de uma calculador simples

--- src/br/net/mfs/PrimeiroPrograma.java
+++ src/br/net/mfs/PrimeiroPrograma.java
@@ -5,14 +5,11 @@ import br.net.mfs.model.Gato;
 public class PrimeiroPrograma {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		/*int a = 2;
-		int b = 2;
-		System.out.println("Hello Word!"+(a + b) ); */
+		
 		Gato gato = new Gato();
 		Livros livros = new Livros();
 		
-		System.out.println(gato ); 
+		System.out.println(gato); 
 		System.out.println(livros);
 
 	}
---
+++ src/br/net/mfs/calculadora/Calculadora.java
@@ -0,0 +1,47 @@
+package br.net.mfs.calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int a, b;
+		System.out.println("Digite o primeiro valor: ");
+		a = scan.nextInt();
+		System.out.println("Digite o segundo valor: ");
+		b = scan.nextInt();
+		
+		int soma = soma(a,b);
+		int subtracao = subtracao(a,b);
+		int multiplicacao = multiplicacao(a,b);
+		double divisao = divisao(a,b);
+		
+		System.out.println("Resultado : ");
+		System.out.println("soma : "+soma);
+		System.out.println("subtracao : "+subtracao);
+		System.out.println("multiplicao : "+multiplicacao);
+		System.out.println("divisao : "+divisao);
+		
+
+	}
+	
+	public static int soma( int a, int b) {
+		return a + b ;
+	}
+	
+	
+	public static int subtracao( int a, int b) {
+		return a - b ;
+	}
+	
+	public static int multiplicacao( int a, int b) {
+		return a * b ;
+	}
+	public static double divisao( int a, int b) {
+		return a / b ;
+	}
+
+}
